e8d6aff5c73d21902c38a4b7f3d9a34b646bc4f2
Cecile Dezy <cecile.dezy@gmail.com>
2019-08-18 14:56:43 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  resources :books do
+    resources :bookings, only: [:index, :show, :new, :create] do
+      resources :reviews, only: [:new, :create]
+    end
+    resources :reviews, only: [:index, :show]
+  end
+
 end
